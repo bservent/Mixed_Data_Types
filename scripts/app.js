d44de9f85a0b0b5707f35e757fb0369c73b103f7
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-09-20 17:54:43 -0700

    bond films gross

--- scripts/app.js
+++ scripts/app.js
@@ -16,7 +16,7 @@ Suppose you are tasked with creating some software, and it is up to **you** to d
 
   * A light switch that can be either on or off. */
 
-/* Boolean
+Boolean
 Ex: 
 light on = true;
 light off = false;
@@ -27,36 +27,36 @@ if (the light is on) {
 
 if (the light is off) {
     console.log('the light is off');
-} */
+}
 /*   * A user's email address. */
 
-/* String
+String
 Ex:
-const email = 'bservent@gmail.com'; */
+const email = 'bservent@gmail.com';
 
 /*   * A spaceship with a hull, laser blasters, tractor beam, and warp drive. */
 
-/* Array
+Array
 Ex:
-var spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']; */
+var spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']; 
 
- /*  * A list of student names from our class. */
+ /*   * A list of student names from our class. */
 
-/*  Array
+ Array
  Ex:
- var classNames = ['thing1', 'thing2', 'thing3', 'thing4']; */
+ var classNames = ['thing1', 'thing2', 'thing3', 'thing4'];
 
- /*  * A list of student names from our class, each with a location. */
-/* 
+/*   * A list of student names from our class, each with a location.  */
+
 String within String
 
-var classNameLocation = [['thing1','oaxaca city'], ['thing2', 'beverly hills'], ['thing3','middle earth'], ['thing4', 'over the rainbow']]; */
+var classNameLocation = [['thing1','oaxaca city'], ['thing2', 'beverly hills'], ['thing3','middle earth'], ['thing4', 'over the rainbow']]; 
  
 /*   * A list of student names from our class, each with a location and each with a list of favorite tv shows.
  */
-/*   Class object  */
+  Class object 
 
-/*   class Person {
+  class Person {
       constructor(name, location, movieList) {
         this.name = name;
         this.location = location;
@@ -64,35 +64,35 @@ var classNameLocation = [['thing1','oaxaca city'], ['thing2', 'beverly hills'], 
       }
   }
 
-  let person1 = new Person ('Britt', 'Seattle','The Departed, Gladiator, Constant Gardener') */
+  let person1 = new Person ('Britt', 'Seattle','The Departed, Gladiator, Constant Gardener')
 
 /* 2. Take it Easy */
 
 /* 1. Make an array that holds all of the colors of the rainbow. */
 
-/* let rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+let rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-console.log(rainbow[4]); */
+console.log(rainbow[4]);
 
 /* 2. Write code that will access "blue" from the rainbow array.
 
 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype. */
 
-/* let person = {
+let person = {
     name: 'Britt',
     favoriteFood: 'Pizza',
     hobby: 'Circus',
     city: 'Seattle',
     favoriteDatatype:'Objects'
-}; */
+};
 
 /* 4. Write code that will access your hobby from the object that you just created. */
 
-/*  console.log(person.hobby); */
+ console.log(person.hobby);
 
 /* 3. Crazy Object! */
 
-/* const crazyObject = {
+const crazyObject = {
   taco: [
     {
       meat: 'steak',
@@ -122,28 +122,28 @@ console.log(rainbow[4]); */
       },
     ]
   }
-} */
+}
 
 
 /* Use crazyObject to log the following. */
 
 /*   * "omg my mouth is burning" */
- /*  console.log(crazyObject.taco[1].salsa[5]); */
+  console.log(crazyObject.taco[1].salsa[5]);
 /*   * "Pretty pretty prettayyyyy good" */
-/*   console.log(crazyObject.larry.quotes[0]); */
+  console.log(crazyObject.larry.quotes[0]);
 /*   * "Swearing at Larry and Jeff" */
-/*   console.log(crazyObject.larry.characters[2].favourtieHobby); */
+  console.log(crazyObject.larry.characters[2].favourtieHobby);
 /*   * "Chicken Teriyaki Boyyyyyy" */
-/*   console.log(crazyObject.larry.nicknames[1]); */
+  console.log(crazyObject.larry.nicknames[1]);
  /*  * The object the contains the name `funkhauser` */
-/*  console.log(crazyObject.larry.characters) */
+ console.log(crazyObject.larry.characters)
 
 /*  4. Object-ception */
 
 /* With the following object: */
 
 
-/* const inception = {
+const inception = {
    reality: {
        dreamLayer1: {
            dreamLayer2: {
@@ -160,11 +160,11 @@ console.log(rainbow[4]); */
        }
    }
 }
- */
+ 
 
 /* Change the value of `limbo` to `null`. */
-/* inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
-console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo); */
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
 
 /* Copy the following `bondFilms` **array** of **objects** above into your js file. Use loops and conditionals and functions in order to complete the below: */
 
@@ -199,20 +199,20 @@ const bondFilms = [
 
 /* 5. Create a new array called `bondTitles` with only the titles of the Bond films, and console.log the new array. */
 
-/* bondTitles = [];
+bondTitles = [];
 
 for (let i = 0; i < bondFilms.length; i++) {
     bondTitles.push(bondFilms[i].title);
 } 
  
-console.log(bondTitles); */
+console.log(bondTitles);
 
 
-/* 6. Create a new array `oddBonds`, of only the Bond films released on odd-numbered years.
+/*  6. Create a new array `oddBonds`, of only the Bond films released on odd-numbered years. */
 
-  &#x1F534; **Commit:** "oddBonds" */
 
-/*   let oddBonds = [];
+
+  let oddBonds = [];
 
 for (let i = 0; i < bondFilms.length; i++) {
     if (bondFilms[i].year % 2 !== 0) {
@@ -220,9 +220,16 @@ for (let i = 0; i < bondFilms.length; i++) {
     }
 }
 
-console.log(oddBonds); */
+console.log(oddBonds);
 
 /* 7. Determine the total cumulative gross of the Bond franchise, and console.log the result. 
   > _Hint_: To make the grosses into usable numbers, look into the `.replace` Javascript method (there are many ways to do this, however). Look into `parseInt()` also.   */ 
-
   
+  let grossSum = 0;
+
+  for (let i = 0; i < bondFilms.length; i++) {
+      const takeOut = bondFilms[i].gross.replace(/[\D]/g, '');
+      grossSum += parseInt(takeOut);
+  };
+
+console.log(grossSum);
